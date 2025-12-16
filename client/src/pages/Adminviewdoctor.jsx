fcb1fd5933ee9f979ff9e073fa949fed2d8c7c49
Resmi S <resmi321@gmail.com>
2025-12-16 06:06:05 +0530

    final 3r commit

--- client/src/pages/Adminviewdoctor.jsx
+++ client/src/pages/Adminviewdoctor.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Adminsidebar from "../components/AdminSidebar";
+import Adminsidebar from "../components/Adminsidebar";
 
 export default function Adminviewdoctor() {
   const [message, setMessage] = useState("");
