f97615078aaed5ec5308c73a5458785a52fb3c73
Resmi S <resmi321@gmail.com>
2025-12-17 11:17:26 +0530

    final 13commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const connectDB = require("./config/db");
 
 
 app.use(express.json());
-app.use(cors());
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
