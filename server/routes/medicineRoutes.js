87e8156cf1defbd5f4771813cfb31ba53bf15365
Resmi S <resmi321@gmail.com>
2025-12-18 15:20:13 +0530

    billing commit

--- server/routes/medicineRoutes.js
+++ server/routes/medicineRoutes.js
@@ -4,7 +4,7 @@ const medicineController = require("../controllers/medicineController");
 
 router.post("/addmedicine", medicineController.addMedicine);
 router.get("/getallmedicine", medicineController.getAllMedicines);
-router.get("getBymedicineId/:id", medicineController.getMedicineById);
+router.get("/getBymedicineId/:id", medicineController.getMedicineById);
 router.put("/:id", medicineController.updateMedicine);
 router.delete("/:id", medicineController.deleteMedicine);
 
