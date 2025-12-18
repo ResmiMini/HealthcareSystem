9a524baebd633a588f27d46e82bd25a54aa74920
Resmi S <resmi321@gmail.com>
2025-12-18 15:07:50 +0530

    billing commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const connectDB = require("./config/db");
 
 
-app.use(express.json());
+
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +22,11 @@ app.use(
   })
 );
 
+app.options("*", cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(async (req, res, next) => {
   try {
@@ -31,6 +36,8 @@ app.use(async (req, res, next) => {
     res.status(500).json({ message: "Database connection failed" });
   }
 });
+
+
 
 app.get("/", (req, res) => {
   res.send("🚀 Backend is running successfully");
