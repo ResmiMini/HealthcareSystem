dcde166361aa007c5ef8f99e9437643ea5cd4ece
Resmi S <resmi321@gmail.com>
2025-12-18 16:46:30 +0530

    billing commit

--- server/config/db.js
+++ server/config/db.js
@@ -6,24 +6,18 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+const connectDB = async () => {
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(process.env.MONGO_URI, {
-        bufferCommands: false,
-      })
-      .then((mongoose) => {
-        console.log("✅ MongoDB connected");
-        return mongoose;
-      });
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
+  console.log("✅ MongoDB connected");
   return cached.conn;
-}
+};
 
 module.exports = connectDB;
