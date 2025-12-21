5e7927212fb41afabc3fcbae39111164cbdb270d
Resmi S <resmi321@gmail.com>
2025-12-21 16:24:24 +0530

    review2 commit

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(
   cors({
-    origin: "https://healthcare-systemfrontend.vercel.app/",
+    origin: "https://healthcare-systemfrontend.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
