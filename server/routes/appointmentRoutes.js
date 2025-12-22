58b60f4cf342133906fe60f68ffe1e627f43cc5e
Resmi S <resmi321@gmail.com>
2025-12-22 19:46:07 +0530

    review  3  commit

--- server/routes/appointmentRoutes.js
+++ server/routes/appointmentRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { bookAppointment } = require("../controllers/appointmentController");
-const {getUpcomingAppointmentsByDoctor, getAppointmentsByPatientId,getAppointmentsByDoctorId,deleteAppointmentById} = require("../controllers/appointmentController");
+const {getAllAppointmentsExceptToday, getAppointmentsByPatientId,getAppointmentsByDoctorId,deleteAppointmentById} = require("../controllers/appointmentController");
 
 router.post("/bookappointment", bookAppointment);
 router.get("/viewappointment/:patientId", getAppointmentsByPatientId);
 router.get("/getByDoctorId/:doctorId",getAppointmentsByDoctorId);
 router.delete("/deleteappointment/:appointmentId", deleteAppointmentById);
-router.get("/getBycomingappDoctorId/:doctorId",getUpcomingAppointmentsByDoctor);
+router.get("/getBycomingappDoctorId/:doctorId",getAllAppointmentsExceptToday);
 
 module.exports = router;
