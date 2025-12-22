b192a01460981f54fd1937d3f87b8d72fcd0cbe7
Resmi S <resmi321@gmail.com>
2025-12-22 11:52:31 +0530

    review 24 commit

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -119,10 +119,10 @@ export default function Payment() {
         
 
   <button
-    onClick={() => deleteAppointment(a.appointmentId)}
+    onClick={() => deleteAppointment(appointmentId)}
     className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600"
   >
-    Delete
+    cancel appointment
   </button>
 
 
